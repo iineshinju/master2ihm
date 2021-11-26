2f1edbd3e10bf8b6763fa6cbd140f2d44bf30505
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-11-26 16:04:52 +0100

    Reorganize pages

--- hobbiesfriends/app/login/inscription/inscription-page.js
+++
@@ -1,6 +0,0 @@
-import { InscriptionViewModel } from './inscription-view-model'
-
-export function onNavigatingTo(args) {
-  const component = args.object
-  component.bindingContext = new LoginViewModel()
-}
--- hobbiesfriends/app/login/inscription/inscription-view-model.js
+++
@@ -1,9 +0,0 @@
-import { fromObject } from '@nativescript/core'
-
-export function InscriptionViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
-
-  return viewModel
-}
