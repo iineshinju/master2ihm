cc4d08423e4d3b415556af853e5fed51ace4135a
iineshinju <perlenolison@gmail.com>
2021-11-26 15:54:55 +0100

    create welcome-page

--- hobbiesfriends/app/welcome/welcome-page.js
+++ hobbiesfriends/app/welcome/welcome-page.js
@@ -4,3 +4,15 @@ export function onNavigatingTo(args) {
   const component = args.object
   component.bindingContext = new WelcomeViewModel()
 }
+
+export function toInscription(args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("../inscription/inscription-page");
+}
+
+export function toConnexion(args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("../login/login-page");
+}
