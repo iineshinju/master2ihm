d8141c45a8122fa63ac22379b03011813bfddca7
iineshinju <perlenolison@gmail.com>
2021-12-02 10:22:22 +0100

    add toPassword()

--- hobbiesfriends/app/login/login-page.js
+++ hobbiesfriends/app/login/login-page.js
@@ -4,3 +4,9 @@ export function onNavigatingTo(args) {
   const component = args.object
   component.bindingContext = new LoginViewModel()
 }
+
+export function toPassword(args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("~/login/password-page");
+}
