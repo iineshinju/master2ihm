140bf778c89c5710a413ba23417013a839219a0a
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-03 17:38:03 +0100

    add toGoogle

--- hobbiesfriends/app/inscription/inscription-page.js
+++ hobbiesfriends/app/inscription/inscription-page.js
@@ -5,6 +5,27 @@ export function onNavigatingTo(args) {
   component.bindingContext = new InscriptionViewModel()
 }
 
+export function toGoogle(args) {
+  const Googlelogin = {
+    title: "Connexion à votre compte Google",
+    message: "Entrez votre adresse gmail et votre mot de passe :",
+    okButtonText: "Connexion",
+    cancelButtonText: "Retour",
+    userNameHint: "Entrez votre adresse gmail",
+    passwordHint: "Entrez votre mot de passe"
+  };
+
+  login(Googlelogin).then((loginResult) => {
+    console.log(loginResult.result);
+    const choice = args.object;
+    const page = choice.page;
+    if (loginResult.result && loginResult.userName != "" && loginResult.password != "") {
+      page.frame.navigate("~/creationProfil/creationProfil-page");
+    }
+  });
+
+}
+
 export function toProfil(args) {
   const button = args.object;
   const page = button.page;
