f4699977757beb7e1ed52c764055bfee9a50e734
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-03 17:27:26 +0100

    add toGoogle

--- hobbiesfriends/app/login/login-page.js
+++ hobbiesfriends/app/login/login-page.js
@@ -10,3 +10,24 @@ export function toPassword(args) {
   const page = button.page;
   page.frame.navigate("~/login/password-page");
 }
+
+export function toGoogle(args) {
+  const Googlelogin = {
+    title: "Connexion à votre compte Google",
+    message: "Entrez votre adresse gmail et votre mot de passe :",
+    okButtonText: "Connexion",
+    cancelButtonText: "Retour",
+    userNameHint: "Entrez votre adresse gmail",
+    passwordHint: "Entrez votre mot de passe"
+  };
+
+  login(Googlelogin).then((loginResult) => {
+    console.log(loginResult.result);
+    const choice = args.object;
+    const page = choice.page;
+    if (loginResult.result && loginResult.userName != "" && loginResult.password != "") {
+      page.frame.navigate("~/home/home-page");
+    }
+  });
+
+}
