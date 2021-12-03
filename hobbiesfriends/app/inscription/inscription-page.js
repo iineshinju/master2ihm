82218c15c3c03e0e8b04696bda87e445cf66fd97
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-03 16:33:24 +0100

    add toLogin

--- hobbiesfriends/app/inscription/inscription-page.js
+++ hobbiesfriends/app/inscription/inscription-page.js
@@ -5,20 +5,14 @@ export function onNavigatingTo(args) {
   component.bindingContext = new InscriptionViewModel()
 }
 
-export function toLogin(args) {
+export function toProfil(args) {
   const button = args.object;
   const page = button.page;
   page.frame.navigate("~/creationProfil/creationProfil-page");
 }
 
-export function alertCreation() {
-  const alertCreation = {
-      title: "Compte crée",
-      message: "Votre compte a été crée",
-      cancelButtonText: "Merci"
-  };
-
-  action(alertCreation).then((result) => {
-      console.log(result);
-  });
+export function toLogin(args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("~/login/login-page");
 }
