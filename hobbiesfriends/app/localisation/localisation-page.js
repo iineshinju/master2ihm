366922401bf55b32366d8497300d5430933c5a82
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-12-05 11:55:36 +0100

    Add toProfil in localisation

--- hobbiesfriends/app/localisation/localisation-page.js
+++ hobbiesfriends/app/localisation/localisation-page.js
@@ -4,5 +4,9 @@ export function onNavigatingTo(args) {
   const component = args.object
   component.bindingContext = new LocalisationViewModel()
 }
-
+export function toProfil(args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("~/creationProfil/creationProfil-page");
+}
 
