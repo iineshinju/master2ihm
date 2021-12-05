7ce746af9a88c55d561043e4ee5ff6bd09786f5b
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-12-05 18:28:13 +0100

    add ListPicker ellement to creationProfil Page

--- hobbiesfriends/app/creationProfil/creationProfil-page.js
+++ hobbiesfriends/app/creationProfil/creationProfil-page.js
@@ -1,18 +1,12 @@
-import { HomeViewModel } from './creationProfil-view-model';
+import { CreationProfilViewModel } from './creationProfil-view-model';
 
-const genre = ["Homme", "Femme", "Autre"];
+
 
 export function onNavigatingTo(args) {
-  const component = args.object
-  component.bindingContext = new HomeViewModel()
+  const items = ["Batman", "Joker", "Bane"];
+   const component = args.object
+  component.bindingContext = new CreationProfilViewModel()
 }
-//export function onListPickerLoaded(fargs) {
- // const listPickerComponent = fargs.object;
- // listPickerComponent.on("selectedIndexChange", (args) => {
- //     const picker = args.object;
-     // console.log(`index: ${picker.selectedIndex}; item" ${genre[picker.selectedIndex]}`);
-  //});
-//}
 
 export function toLocalisation(args) {
   const button = args.object;
--- hobbiesfriends/app/creationProfil/creationProfil-view-model.js
+++ hobbiesfriends/app/creationProfil/creationProfil-view-model.js
@@ -1,9 +1,11 @@
 import { fromObject } from '@nativescript/core'
+var observableModule = require("/node_modules/@nativescript/core/data/observable"); // to install @nativescript, run :  npm install @nativescript/core
 
-export function HomeViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
+export function CreationProfilViewModel() {
+  var viewModel = observableModule.fromObject({
+    genre : ["Homme", "Femme", "Autre"],
+    selectedListPickerIndex: 0,
+  });
 
-  return viewModel
+  return viewModel;
 }
