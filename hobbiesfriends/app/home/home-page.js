a31f39b6fb07600f5e0581cc955feeb40d57f01e
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-08 11:09:29 +0100

    add function onItemTap

--- hobbiesfriends/app/home/home-page.js
+++ hobbiesfriends/app/home/home-page.js
@@ -4,3 +4,20 @@ export function onNavigatingTo(args) {
   const component = args.object
   component.bindingContext = new HomeViewModel()
 }
+
+export function onItemTap(args) {
+  const view = args.view
+  const page = view.page
+  const tappedItem = view.bindingContext
+
+  page.frame.navigate({
+    moduleName: 'home/home-item-detail/home-item-detail-page',
+    context: tappedItem,
+    animated: true,
+    transition: {
+      name: 'slide',
+      duration: 200,
+      curve: 'ease',
+    },
+  })
+}
